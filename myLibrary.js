972bb4295a06fd03a8a07eee57d343c779fe7061
Abul Hasan <abulhasan@hztech.biz>
2024-07-04 08:43:55 -0700

    added another function

--- myLibrary.js
+++ myLibrary.js
@@ -1,10 +1,20 @@
 
 (function(global) {
     const myLibrary = {};
-
     myLibrary.sayHello = function(name) {
         return `Hello, ${name}!`;
     };
 
+    myLibrary.generateTable = function(number) {
+        let table = '';
+        for (let i = 1; i <= 10; i++) {
+            table += `${number} × ${i} = ${number * i}\n`;
+        }
+        return table;
+    };
+
     global.myLibrary = myLibrary;
 })(typeof window !== 'undefined' ? window : global);
+
+
+// npx uglify-js myLibrary.js -o myLibrary.min.js
